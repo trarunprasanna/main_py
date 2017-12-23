2ed2c207f653b4f70513b94a5ec951de1d5b76df
trarunprasanna <trarunprasanna@gmail.com>
2017-12-23 23:00:23 +0530

    sample program

--- py/add.py
+++ py/add.py
@@ -1,6 +1,6 @@
 a=input("enter the first number")
 b=input("enter the second number")
 
-c= float(a)+float(b)
+c= a+b
 
 print (c)
--- py/div.py
+++ py/div.py
@@ -1,6 +1,6 @@
 a=input("enter the first number")
 b=input("enter the second number")
 
-c= float(a)+float(b)
+c= float(a) - float(b)
 
 print (c)
