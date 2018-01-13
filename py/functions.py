877034825cc1cba9d72bd254e63036e457387793
trarunprasanna <trarunprasanna@gmail.com>
2018-01-13 19:30:56 +0530

    add function

---
+++ py/functions.py
@@ -0,0 +1,17 @@
+print ("hello")
+x=1
+y=1
+
+if x==1:
+    print ("yes the value of x is 1")
+    if y==2:
+        print ("yes y value is 2")
+    else:
+        print (" y value is diffrent")
+else:
+    print("value of x is not 1")
+
+
+
+
+
