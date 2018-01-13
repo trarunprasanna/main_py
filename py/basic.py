3d7840d128da195ed4e3cb5dde62bfde14bc3144
trarunprasanna <trarunprasanna@gmail.com>
2018-01-13 22:01:47 +0530

    basic

---
+++ py/basic.py
@@ -0,0 +1,28 @@
+i=10
+print (type(i))
+
+#string slice
+
+str1='hello'
+str2='how are you'
+
+print (str1[2:4])#it will print the 2 prsition and 3 position char
+print (str2[:-3]) # it will exclude the last 3 char
+print (str2[-3:]) # it will print the last 3 and remainign
+print (str2[-3]) # it will print the last 3 char only
+
+#list
+list1=[1,2,3,4,5]
+print (list1)
+
+list2=['a','b','c']
+print (list2)
+print (type(list1))
+print (dir(list1))
+
+#tuple
+tuple1 =(1,2,3,)
+print (tuple1)
+
+tuple2 =('a','b','c')
+print (tuple2)
