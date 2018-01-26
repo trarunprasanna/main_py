9fcd5b596c17ef3c0f7df074b8e58de728ee10f1
trarunprasanna <trarunprasanna@gmail.com>
2018-01-26 12:13:15 +0530

    import

---
+++ py/import.py
@@ -0,0 +1,14 @@
+import random
+
+
+'''for i in range (0,20):
+    health = random.randint(1,10)
+    print (i, health)
+'''
+string = "Hello"
+for x in string:
+    print(x)
+
+
+# print (python -V)
+
