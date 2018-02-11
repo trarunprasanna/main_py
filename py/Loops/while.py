0c74a479cb65dc5126c5591f68240acb38444b45
ArunPrasanna <trarunprasanna@gmail.com>
2018-02-11 10:37:01 +0530

    DS, Loops

--- py/Loops/while.py
+++ py/Loops/while.py
@@ -1,2 +1,7 @@
-while True :
-    print ("hello")
+
+even_numbers = [x for  x in range(1,101) if x % 2 ==0]
+print (even_numbers)
+
+
+odd_numbers = [x for  x in range(1,101) if x % 2 != 0]
+print (odd_numbers)
